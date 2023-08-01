910e1877c75b64073a5ece12d89cfefbcaf1f710
AkiraRei <Akirarei@yandex.ru>
2023-08-01 15:53:44 +0300

    Find a problem and fix. Final

--- AnimationsProjectHW/Model/AnimationsList.swift
+++ AnimationsProjectHW/Model/AnimationsList.swift
@@ -40,6 +40,42 @@ struct AnimationsStore {
         .swing,
     ]
     
-    let curves = AnimationCurve.AllCases()
+    let curves: [AnimationCurve] = [
+    
+        .easeIn,
+        .easeOut,
+        .easeInOut,
+        .linear,
+        .spring,
+        .easeInSine,
+        .easeOutSine,
+        .easeInOutSine,
+        .easeInQuad,
+        .easeOutQuad,
+        .easeInOutQuad,
+        .easeInCubic,
+        .easeOutCubic,
+        .easeInOutCubic,
+        .easeInQuart,
+        .easeOutQuart,
+        .easeInOutQuart,
+        .easeInQuint,
+        .easeOutQuint,
+        .easeInOutQuint,
+        .easeInExpo,
+        .easeOutExpo,
+        .easeInOutExpo,
+        .easeInCirc,
+        .easeOutCirc,
+        .easeInOutCirc,
+        .easeInBack,
+        .easeOutBack,
+        .easeInOutBack,
+    
+    ]
+    
+//    let curves = AnimationCurve.AllCases()
+//    какого черта они не работают? ._.
+//    let test = AnimationPreset.AllCases()
     
 }
